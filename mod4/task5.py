4c55e7b445b329bed8656ebb4ea4a0f979ba28e2
CreatFun <129187437+CreatFun@users.noreply.github.com>
2023-10-19 23:36:23 +0500

    homework 4 task 5 fixed

--- mod4/task5.py
+++ mod4/task5.py
@@ -8,9 +8,17 @@ def get_amount_of_letters(string):
     return letters_and_amount
 
 
-string = input()
+file_name = input()
+file = open(file_name + ".txt", "r", encoding="utf-8")
+string_from_file = file.read()
+file.close()
 
-dict_letters_amount = get_amount_of_letters(string)
+string_for_check = ""
+for sym in string_from_file:
+    if sym.isalpha():
+        string_for_check += sym
+
+dict_letters_amount = get_amount_of_letters(string_for_check)
 sorted_list = sorted(dict_letters_amount.items(), key=lambda x: x[1])
 
 result_file = open("result_task5.txt", "w", encoding="utf-8")
